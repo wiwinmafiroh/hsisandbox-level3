7e3d789457479fc2748ba2b2965130049dfe22ab
Wiwin Mafiroh <wiwinmafiroh@gmail.com>
2022-03-16 14:30:50 +0700

    Update databases

--- hsi_sandbox_lvl3/settings.py
+++ hsi_sandbox_lvl3/settings.py
@@ -83,8 +83,7 @@ WSGI_APPLICATION = 'hsi_sandbox_lvl3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 # if os.environ.get('DATABASE_URL'):
